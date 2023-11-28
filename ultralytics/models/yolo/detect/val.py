5aee66d162bf0dabe418ac6891568cd0691e9cee
sairahann <152267559+sairahann@users.noreply.github.com>
2023-11-28 09:50:30 +0000

    Update val.py

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -74,7 +74,7 @@ class DetectionValidator(BaseValidator):
 
     def get_desc(self):
         """Return a formatted string summarizing class metrics of YOLO model."""
-        return ('%22s' + '%11s' * 8) % ('Class', 'Images', 'Instances', 'Box(P', 'R', 'F1-Score', 'mAP50', 'mAP50-95)')
+        return ('%22s' + '%11s' * 7) % ('Class', 'Images', 'Instances', 'Box(P', 'R', 'F1-Score', 'mAP50', 'mAP50-95)')
 
     def postprocess(self, preds):
         """Apply Non-maximum suppression to prediction outputs."""
